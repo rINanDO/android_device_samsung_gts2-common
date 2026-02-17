c78414d99628c1e4c7aaa53e2b4f77d74c82d010
Dominggoes Isakh <drjisakh@gmail.com>
2026-02-17 15:47:54 +0100

    gts2-common: Use soong_config_set for Lineage Health
    Change-Id: Ic77cc96b1d101ea76af11d52e7d41c64ce64ebc4

--- device-common.mk
+++ device-common.mk
@@ -145,6 +145,11 @@ PRODUCT_PACKAGES += \
     libion
 
 # Lineage Health
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/batt_slate_mode)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
+
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
